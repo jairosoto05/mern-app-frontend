fa8c6727978a764059b2c4d4dca2a3285ab27d6c
Jairo Soto <jairosoto05@gmail.com>
2021-03-28 01:41:01 -0400

    rutas creadas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 
+import Navigation from './components/Navigation'
+import NotesList from './components/NotesList'
+import CreateNote from './components/CreateNote'
+import CreateUser from './components/CreateUser'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation/>
+      <Route exact path='/' component={NotesList}/>
+      <Route exact path='/edit/:id' component={<CreateNote}/>
+      <Route exact path='/create' component={CreateNote}/>
+      <Route exact path='/user' component={CreateUser}/>
+      hello world
+    </Router>
+      
   );
 }
 
